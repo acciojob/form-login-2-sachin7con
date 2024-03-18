e04a2bf207f751adb5c0fe228f0ea6e820d6aa30
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-18 11:24:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+function ValFunc(){
+	const fName = document.getElementById("FirstName");
+	const lName = document.getElementById("LastName");
+	const pNum = document.getElementById("PhoneNumber");
+	const email = document.getElementById("Email");
+
+	alert(fName.name + ": " + fName.value +  " " + lName.name + ": " + lName.value + " " + pNum.name + ": " + pNum.value + " " + email.name + ": "  + email.value );
+}
